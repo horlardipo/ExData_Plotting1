a0bcc4e3a6f114544c62a195ec539bf474cb8a83
horlardipo <65602101+horlardipo@users.noreply.github.com>
2020-08-24 14:54:11 +0300

    R code

---
+++ plot3.R
@@ -0,0 +1,23 @@
+##download zip and extract file to desktop in order to read the file
+
+read_data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+dim(read_data)
+
+
+
+finalData <- read_data[read_data$Date %in% c("1/2/2007","2/2/2007"),]
+finalData
+
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+
+##Plot 3
+columnlines <- c("black", "red", "blue")
+labels <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+plot(finalData$SetTime, finalData$Sub_metering_1, type="l", col=columnlines[1], xlab="", ylab="Energy sub metering")
+lines(finalData$SetTime, finalData$Sub_metering_2, col=columnlines[2])
+lines(finalData$SetTime, finalData$Sub_metering_3, col=columnlines[3])
+legend("topright", legend=labels, col=columnlines, lty="solid")
+
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
